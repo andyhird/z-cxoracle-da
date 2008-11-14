f92b37ad39cd644f2cf3c6978a9dba84925bf5f0
andyhird <andyhird@75bc2130-b25e-11dd-8714-116ca565c52f>
2008-11-14 15:16:43 +0000

    0.5 release code

---
+++ trunk/__init__.py
@@ -0,0 +1,22 @@
+##############################################################################
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+__doc__="ZcxOracle Database Adaptor Registration."
+__version__='$Revision: 0.5 $'[11:-2]
+
+import DA
+
+def initialize(context):
+    context.registerClass(
+        DA.Connection,
+        permission = 'Add Z cxOracle Database Connections',
+        constructors = (DA.manage_addZcxOracleConnectionForm,
+                        DA.manage_addZcxOracleConnection),
+        icon = SOFTWARE_HOME + '/Shared/DC/ZRDB/www/DBAdapterFolder_icon.gif')
